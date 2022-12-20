d147e523158291123601adbe29b9178205de2f11
摘新 <shuanghua.msh@alibaba-inc.com>
2022-12-20 15:08:47 +0800

    feat(): init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1671519326925_1626',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
